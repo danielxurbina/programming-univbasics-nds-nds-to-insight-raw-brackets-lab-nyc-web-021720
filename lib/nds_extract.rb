7a17a9c2a1c155db24c1707d182e0e469eee2894
Daniel Urbina <danielur1999@gmail.com>
2020-02-12 04:02:34 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-pp directors_database
+#pp directors_database
 
 def directors_totals(nds)
 result = {}
@@ -15,7 +15,7 @@ while director_index < nds.length do
 
 end
 
-result
+p result
 
 end
 
